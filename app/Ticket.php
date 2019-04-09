574e92b0d556365e81f8415e145ea715f9f50fe2
viparvez <viparvez@gmail.com>
2019-04-09 18:39:53 +0600

    Updated ticket details view

--- app/Ticket.php
+++ app/Ticket.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ticket extends Model
 {
-    //
+    public function Createdby() {
+    	return $this->belongsTo('App\User', 'createdbyuser_id');
+    }
+
+    public function Updatedby() {
+    	return $this->belongsTo('App\User', 'updatedbyuser_id');
+    }
 }
