ae601bf0d11652d85bdc8b5fb29bc26f0590ce5d
Sirajum Monir Parvez <sirajum.monir.ete@ulab.edu.bd>
2019-04-18 12:59:28 +0600

    Create TicketleadController.php

---
+++ app/Http/Controllers/TicketleadController.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Redirect;
+use App\Ticketlead;
+use Auth;
+use Validator;
+
+
+class TicketleadController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'comment' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return Redirect::back()->withErrors($validator);
+        }
+
+        DB::beginTransaction();
+        
+        try {
+            
+            $id = DB::table('ticketleads')->insertGetId(
+                [
+                    'lead' => $request->comment,
+                    'leadtype' => 'COMMENT',
+                    'ticket_id' => $request->ticket_id,
+                    'created_at' => date('Y-m-d h:i:s'),
+                    'updated_at' => date('Y-m-d h:i:s'),
+                    'createdbyuser_id' => Auth::user()->id,
+                    'updatedbyuser_id' => Auth::user()->id,
+                ]
+            );
+
+            DB::commit();
+            return redirect()->back()->with('message', "Successful");
+   
+        } catch (\Exception $e) {
+          DB::rollback();
+          return $e;
+          return response()->json(['error'=>array('Could not add comment')]);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
